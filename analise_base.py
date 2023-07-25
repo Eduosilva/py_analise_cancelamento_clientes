a72346441e46b13dec9f5cd5e658faaf88cd07e7
Eduosilva <63400831+Eduosilva@users.noreply.github.com>
2023-07-24 22:27:16 -0300

    Update analise_base.py

--- analise_base.py
+++ analise_base.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+tabela = pd.read_csv("cancelamentos.csv")
+tabela = tabela.drop("CustomerID", axis=1)
+display(tabela)
+
+# identificando e removendo valores vazios
+display(tabela.info())
+tabela = tabela.dropna()
+display(tabela.info())
+
+# quantas pessoas cancelaram e não cancelaram
+display(tabela["cancelou"].value_counts())
+display(tabela["cancelou"].value_counts(normalize=True).map("{:.1%}".format))
+
+display(tabela["duracao_contrato"].value_counts(normalize=True))
+display(tabela["duracao_contrato"].value_counts())
